ee41ef6b34e3941d2f00be90bc4a6897ec5600a5
Marcos Rian <marcosriam10@gmail.com>
2024-03-29 15:20:12 -0300

    Starting the project

---
+++ README.md
@@ -0,0 +1,7 @@
+# todo_list
+
+A todo list using an external api
+
+## About this project
+
+This project is an activity of the flutterando course.
---
+++ android/app/src/main/kotlin/com/nascdev/todo_list_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nascdev.todo_list_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
