a4c70c7e6e5759e91990a4aa07e54cac078c1855
Marcos Rian <marcosriam10@gmail.com>
2024-03-31 16:22:37 -0300

    api page

--- lib/src/views/api_page.dart
+++ lib/src/views/api_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
-class ApiPage extends StatelessWidget {
+class ApiPage extends StatefulWidget {
   const ApiPage({super.key});
 
   @override
+  State<ApiPage> createState() => _ApiPageState();
+}
+
+class _ApiPageState extends State<ApiPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Api Page'),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return const Text('data');
+        },
+      ),
+    );
   }
 }
